e733343294ea0732362da811e97afcd190b6805e
kejunglama <kejunglama@gmail.com>
2024-04-03 11:59:48 +0200

    Cleanup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import React from "react";
 import "./App.css";
 import Catalog from "./components/Catalog";
+import Navbar from "./components/Navbar";
 
 function App() {
   const items = require("./assets/products.json");
 
   return (
     <div className="App">
-      <img
-        className="App-logo"
-        src="https://global.fr.shopping.rakuten.com/wp-content/uploads/2019/08/Recurso-15.svg"
-        alt="Rakuten Logo"
-      />
+      <Navbar />
       <Catalog items={items} />
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar: React.FC = () => {
+  return (
+    <div>
+      <img
+        className="App-logo"
+        src="https://global.fr.shopping.rakuten.com/wp-content/uploads/2019/08/Recurso-15.svg"
+        alt="Rakuten Logo"
+      />
+    </div>
+  );
+};
+
+export default Navbar;
